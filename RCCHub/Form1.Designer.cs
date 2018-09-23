6186c83faf67db29636e9b9df6894bff25d42cb6
goufix <alifer.17.11@gmail.com>
2018-09-23 00:52:37 -0300

    implementações

--- RCCHub/Form1.Designer.cs
+++ RCCHub/Form1.Designer.cs
@@ -29,12 +29,121 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.txtTAG = new System.Windows.Forms.TextBox();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            // 
+            // bunifuSeparator1
+            // 
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(375, 173);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(100, 20);
+            this.txtNick.TabIndex = 2;
+            // 
+            // txtTAG
+            // 
+            this.txtTAG.Location = new System.Drawing.Point(375, 199);
+            this.txtTAG.Name = "txtTAG";
+            this.txtTAG.Size = new System.Drawing.Size(100, 20);
+            this.txtTAG.TabIndex = 3;
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.Location = new System.Drawing.Point(375, 225);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(100, 20);
+            this.txtPatente.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(762, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nick:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "TAG:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Patente:";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(386, 251);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.TabIndex = 9;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPatente);
+            this.Controls.Add(this.txtTAG);
+            this.Controls.Add(this.txtNick);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.TextBox txtTAG;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Login;
     }
 }
 
